d9865653b3a8bf83ebc5110f28135b2946d91cc2
veritasUF <1152130129@stu.hit.edu.cn>
2019-01-13 23:54:15 +0800

    as changed 1/13

--- SurfTest.py
+++ SurfTest.py
@@ -1,31 +1,55 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.core.multiarray import ndarray
+from sklearn.cluster import KMeans
+from sklearn import metrics
 from myfuncs import *
 
-img1 = cv.imread('E:/Downloads/forest_1986.png',cv.IMREAD_GRAYSCALE)
-img2 = cv.imread('E:/Downloads/forest_1992.png',cv.IMREAD_GRAYSCALE)
+img1 = cv.imread('E:/Downloads/forest_1992.png', cv.IMREAD_GRAYSCALE)
+img2 = cv.imread('E:/Downloads/forest_1986.png', cv.IMREAD_GRAYSCALE)
 
 img3, img4 = reg(img1, img2)
-
 '''
+plt.subplot(221)
+plt.imshow(img1, 'gray')
+plt.subplot(222)
+plt.imshow(img2, 'gray')
+plt.subplot(223)
+plt.imshow(img3, 'gray')
+plt.subplot(224)
+plt.imshow(img4, 'gray'), plt.show()
+
+delta = abs(img3-img4)
+Z = delta.reshape(-1, 1)
+y_pred = MiniBatchKMeans(n_clusters=k, batch_size = 200, random_state=9).fit_predict(Z)
+score= metrics.calinski_harabaz_score(Z, y_pred)
+
+
 a = Img_PCA(CD_diff(img3,img4))
-print(a)
-ret,a = cv.threshold(a,127,255,cv.THRESH_BINARY)
-print(a)
+ret,a = cv.threshold(a,1,255,cv.THRESH_BINARY)
 '''
 
 a = Img_PCA(diff(img3, img4))
-b = diff(Img_PCA(img3), Img_PCA(img4))
+
+# kernel = np.ones((3, 3), np.uint8)
+# if a.sum() > a.size/2:
+#     a = np.float32(127 * a)
+#     a = cv.morphologyEx(a, cv.MORPH_CLOSE, kernel)
+# else:
+#     a = np.float32(127 * a)
+#     a = cv.morphologyEx(a, cv.MORPH_OPEN, kernel)
 
 plt.figure()
 plt.subplot(221)
-plt.imshow(img3, 'gray')
-plt.subplot(222)
-plt.imshow(img4, 'gray')
+plt.imshow(img1, 'gray')
 plt.subplot(223)
-plt.imshow(a, 'gray')
+plt.imshow(img2, 'gray')
+plt.subplot(222)
+plt.imshow(img3, 'gray')
 plt.subplot(224)
-plt.imshow(b, 'gray')
+plt.imshow(img4, 'gray')
+plt.figure()
+plt.imshow(a, 'gray')
 
 plt.show()
--- myfuncs.py
+++ myfuncs.py
@@ -3,70 +3,75 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-#配准算法
-#依赖于opencv库
-#暂为面向过程形式，未来有机会（并没有）可能改写成OOP形式
+# 配准算法
+# 依赖于opencv库
+# 暂为面向过程形式，未来有机会（并没有）可能改写成OOP形式
 
 surf = cv.xfeatures2d.SIFT_create()
-#surf = cv.xfeatures2d.SURF_create(2)
+
+# surf = cv.xfeatures2d.SURF_create(100)
 
 def reg(img1, img2):
-    kp1,des1 = surf.detectAndCompute(img1,None)
-    kp2,des2 = surf.detectAndCompute(img2,None)
+    kp1, des1 = surf.detectAndCompute(img1, None)
+    kp2, des2 = surf.detectAndCompute(img2, None)
 
     bf = cv.BFMatcher()
-    matches = bf.knnMatch(des1,des2, k=2)
+    matches = bf.knnMatch(des1, des2, k=2)
 
     good = []
-    for m,n in matches:
-        if m.distance < 0.6*n.distance: #阈值是怎么确定的
+    for m, n in matches:
+        if m.distance < 0.5 * n.distance:  # 阈值是怎么确定的
             good.append([m])
 
-    src_pts = np.float32([ kp1[m[0].queryIdx].pt for m in good ])
-    dst_pts = np.float32([ kp2[m[0].trainIdx].pt for m in good ])
+    src_pts = np.float32([kp1[m[0].queryIdx].pt for m in good])
+    dst_pts = np.float32([kp2[m[0].trainIdx].pt for m in good])
 
-    src_val = [ img1.item(int(kp1[m[0].queryIdx].pt[1]), int(kp1[m[0].queryIdx].pt[0])) for m in good ]
-    dst_val = [ img2.item(int(kp2[m[0].trainIdx].pt[1]), int(kp2[m[0].trainIdx].pt[0])) for m in good ]
+    src_val = [img1.item(int(kp1[m[0].queryIdx].pt[1]), int(kp1[m[0].queryIdx].pt[0])) for m in good]
+    dst_val = [img2.item(int(kp2[m[0].trainIdx].pt[1]), int(kp2[m[0].trainIdx].pt[0])) for m in good]
 
-    #辐射配准
+    img9 = cv.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=2)
+    plt.imshow(img9), plt.show()
+
+    # 辐射配准
     z1 = np.polyfit(dst_val, src_val, 1)
     p1 = np.poly1d(z1)
-    #print(p1)
-    M, mask = cv.findHomography(dst_pts, src_pts)
+    # print(p1)
+    M, mask = cv.findHomography(src_pts, dst_pts)
 
-    img3 = cv.warpPerspective(img1,M,img2.shape)
+    img3 = cv.warpPerspective(img1, M, (img2.shape[1], img2.shape[0]))
     img4 = np.round(p1(img2))
-    return img3,img4
 
-#PCA与作差算法，作差算法中集成了Kmeans聚类
+    return img3, img4
 
-def diff(img1,img2):
 
-    delta=abs(img2-img1)
+# PCA与作差算法，作差算法中集成了Kmeans聚类
 
-    Z = delta.reshape(-1,1)
+def diff(img1, img2):
+    delta = abs(img2 - img1)
+
+    Z = delta.reshape(-1, 1)
     res = KMeans(n_clusters=2, random_state=16).fit_predict(Z)
-    res2 = res.reshape((delta.shape))
+    res2 = res.reshape(delta.shape)
 
     return res2
 
+
 def Img_PCA(delta):
-    U,S,V=np.linalg.svd(delta)
-    SS=np.zeros(U.shape)
+    U, S, V = np.linalg.svd(delta)
+    SS = np.zeros(U.shape)
     for i in range(S.shape[0]):
-        SS[i][i]=S[i]
+        SS[i][i] = S[i]
 
-    def Pick_k(S):
-        sval=np.sum(S)
-        for i in range(S.shape[0]):
-            if np.sum(S[:i])>=0.6*sval:
+    def Pick_k(s):
+        sval = np.sum(s)
+        for i in range(s.shape[0]):
+            if np.sum(s[:i]) >= 0.6 * sval:
                 break
-        return i+1
+        return i + 1
 
-    k=Pick_k(S)
-    Uk=U[:,0:k]
-    Sk=SS[0:k,0:k]
-    Vk=V[0:k,:]
-    im=np.dot(np.dot(Uk,Sk),Vk)
-    return np.round(im)
-
+    k = Pick_k(S)
+    Uk = U[:, 0:k]
+    Sk = SS[0:k, 0:k]
+    Vk = V[0:k, :]
+    im = np.dot(np.dot(Uk, Sk), Vk)
+    return np.int8(np.round(im))
