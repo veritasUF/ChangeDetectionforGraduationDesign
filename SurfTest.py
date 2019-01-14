5a047c1425c57d15132b1a3e25e43b465138378c
veritasUF <1152130129@stu.hit.edu.cn>
2019-01-15 01:18:11 +0800

    test on HEB, add results

--- SurfTest.py
+++ SurfTest.py
@@ -5,9 +5,12 @@ from numpy.core.multiarray import ndarray
 from sklearn.cluster import KMeans
 from sklearn import metrics
 from myfuncs import *
+from osgeo import gdal
 
-img1 = cv.imread('E:/Downloads/forest_1992.png', cv.IMREAD_GRAYSCALE)
-img2 = cv.imread('E:/Downloads/forest_1986.png', cv.IMREAD_GRAYSCALE)
+img1 = cv.imread('E:/Downloads/D11.png', cv.IMREAD_GRAYSCALE)
+img2 = cv.imread('E:/Downloads/D12.png', cv.IMREAD_GRAYSCALE)
+
+# img2 = np.uint8(np.float32(img2) * (img1.sum()/img1.size*img2.size/img2.sum()))
 
 img3, img4 = reg(img1, img2)
 '''
@@ -32,24 +35,24 @@ ret,a = cv.threshold(a,1,255,cv.THRESH_BINARY)
 
 a = Img_PCA(diff(img3, img4))
 
-# kernel = np.ones((3, 3), np.uint8)
-# if a.sum() > a.size/2:
-#     a = np.float32(127 * a)
-#     a = cv.morphologyEx(a, cv.MORPH_CLOSE, kernel)
-# else:
-#     a = np.float32(127 * a)
-#     a = cv.morphologyEx(a, cv.MORPH_OPEN, kernel)
+kernel = np.ones((3, 3), np.uint8)
+if a.sum() > a.size/2:
+    a = np.float32(127 * a)
+    a = cv.morphologyEx(a, cv.MORPH_CLOSE, kernel)
+else:
+    a = np.float32(127 * a)
+    a = cv.morphologyEx(a, cv.MORPH_OPEN, kernel)
 
 plt.figure()
-plt.subplot(221)
+plt.subplot(231)
 plt.imshow(img1, 'gray')
-plt.subplot(223)
+plt.subplot(234)
 plt.imshow(img2, 'gray')
-plt.subplot(222)
+plt.subplot(232)
 plt.imshow(img3, 'gray')
-plt.subplot(224)
+plt.subplot(235)
 plt.imshow(img4, 'gray')
-plt.figure()
+plt.subplot(133)
 plt.imshow(a, 'gray')
 
 plt.show()
--- myfuncs.py
+++ myfuncs.py
@@ -9,7 +9,7 @@ from sklearn.cluster import KMeans
 
 surf = cv.xfeatures2d.SIFT_create()
 
-# surf = cv.xfeatures2d.SURF_create(100)
+# surf = cv.xfeatures2d.SURF_create(20)
 
 def reg(img1, img2):
     kp1, des1 = surf.detectAndCompute(img1, None)
@@ -20,7 +20,7 @@ def reg(img1, img2):
 
     good = []
     for m, n in matches:
-        if m.distance < 0.5 * n.distance:  # 阈值是怎么确定的
+        if m.distance < 0.6 * n.distance:  # 阈值是怎么确定的
             good.append([m])
 
     src_pts = np.float32([kp1[m[0].queryIdx].pt for m in good])
